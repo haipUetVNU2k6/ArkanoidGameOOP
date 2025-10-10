b361886c2ec6f83db2cd464a2ed10a397e93983a
haip_ltnc2025 <222034401+haipUetVNU2k6@users.noreply.github.com>
2025-10-10 22:40:48 +0700

    Update class Ball
    add methods intersect

--- src/main/java/Game/Object/Ball.java
+++ src/main/java/Game/Object/Ball.java
@@ -9,7 +9,10 @@ public class Ball extends  MovableObject{
     private int speed;
     private double directionX;
     private double directionY;
-
+    public enum Direction{
+        top,down,
+        left,right;
+    }
     public Ball(double x,double y,double radius,int speed,double directionX,double directionY) {
         super(x,y,2*radius,2*radius,directionX*speed,directionY*speed);
         this.speed = speed;
@@ -30,7 +33,15 @@ public class Ball extends  MovableObject{
     public void setDirectionY(double y) {
         this.directionY = y;
     }
-    public void bounceOff(GameObject object) {
+
+    public Direction intersect(GameObject obj) {
+        double overlapTop = obj.getY() + obj.getHeight() - (this.getY() - this.getHeight());
+        double overlapDown = obj.getY() - (this.getY()+this.getHeight());
+        double overlapLeft = obj.getX() - (this.getX()+this.getWidth());
+        double overlapRight = obj.getX() + obj.getWidth() - (this.getX() - this.getWidth());
+    }
+
+    public void bounceOff(Direction dir) {
 
     }
     @Override
