6e80a797d059163b4db226a571ec9513ee42845d
LongRonaldo <tl1212006@gmail.com>
2025-10-17 01:15:13 +0700

    change PauseColor

--- src/main/java/com/example/arkanoidProject/state_controller/state/PauseState.java
+++ src/main/java/com/example/arkanoidProject/state_controller/state/PauseState.java
@@ -16,8 +16,6 @@ public class PauseState extends State {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/arkanoidProject/fxml/pause.fxml"));
             root = loader.load();
-//            root.getStylesheets().add(getClass().getResource("/com/example/arkanoidProject/css/style.css").toExternalForm());
-//            root.getStylesheets().add(getClass().getResource("/com/example/arkanoidProject/css/pause.css").toExternalForm());
         } catch (IOException e) {
             e.printStackTrace();
         }
